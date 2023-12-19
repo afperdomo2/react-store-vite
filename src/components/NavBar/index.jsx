a6e34e6bdc8de8b19b8bb4d7aa5085807cf822ca
alvarado0925 <alvarado0925@gmail.com>
2023-12-18 22:30:00 -0500

    🚀Refactor: Refactoriza los filtros del NavBar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -2,6 +2,14 @@ import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   const activeStyle = "underline underline-offset-4";
+  const filtersRoutes = [
+    "all",
+    "clothes",
+    "electronics",
+    "furnitures",
+    "toys",
+    "others",
+  ];
 
   return (
     <nav className="w-full flex justify-between items-center fixed z-10 py-5 px-5 text-sm font-light">
@@ -9,54 +17,16 @@ const NavBar = () => {
         <li className="font-semibold text-lg">
           <NavLink to="/">Shopi</NavLink>
         </li>
-        <li>
-          <NavLink
-            to="/all"
-            className={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            All
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/clothes"
-            className={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            Clothes
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/electronics"
-            className={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            Electronics
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/furnitures"
-            className={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            Furnitures
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/toys"
-            className={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            Toys
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/others"
-            className={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            Others
-          </NavLink>
-        </li>
+        {filtersRoutes.map((route) => (
+          <li key={route}>
+            <NavLink
+              to={`/${route}`}
+              className={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              {route.charAt(0).toUpperCase() + route.slice(1)}
+            </NavLink>
+          </li>
+        ))}
       </ul>
 
       <ul className="flex items-center gap-4">
